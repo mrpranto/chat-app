a057d809442d070ee21d7e9ab5baf6054de94878
mrpranto <prantoabir1@gmail.com>
2024-06-29 12:05:16 +0600

    uploading file in mern stack

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,10 @@
+// external imports
+const bcrypt = require("bcrypt");
+
+// internal imports 
+
+const User = require("../models/People");
+
 
 // Get users page
 
@@ -7,6 +14,46 @@ function getUsers(req, res, next){
 }
 
 
+async function addUser(req, res, next){
+    let newUser;
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    if(req.files && req.files.length > 0){
+        newUser = new User({
+            ...req.body,
+            avatar: req.files[0].filename,
+            password: hashedPassword,
+        });
+    }else{
+        newUser = new User({
+            ...req.body,
+            password: hashedPassword,
+        });
+    }
+
+    // save user or send error
+
+    try{
+
+        const result = await newUser.save();
+
+        res.status(200).json({
+            message: "User was added successful!",
+        })
+
+    }catch(err){
+
+        res.status(500).json({
+            common:{
+                msg: "Unknown error occured!",
+            }
+        })
+    }
+}
+
+
+
 module.exports = {
-    getUsers
+    getUsers,
+    addUser
 }
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,15 +1,19 @@
 const express = require("express");
-
+const {check} = require("express-validator");
 
 // Internal Imports
 
-const {getUsers} = require("../controller/userController");
+const {getUsers, addUser} = require("../controller/userController");
 const decorateHtmlResponse = require("../middleware/common/decorateHtmlResponse");
+const avatarUpload = require("../middleware/users/avatarUpload");
+const {addUserValidators, addUserValidationHandler} = require("../middleware/users/userValidators");
 
 const router = express.Router();
 
 
 router.get("/", decorateHtmlResponse('Users'), getUsers);
+
+router.post("/", avatarUpload, addUserValidators, addUserValidationHandler, addUser);
 
 
 module.exports = router;
